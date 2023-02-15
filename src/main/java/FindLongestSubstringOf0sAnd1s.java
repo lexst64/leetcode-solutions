00136dd5ebec45dc4beb67b196dd8a6d7a402d72
lexst64 <alexey.st.official@gmail.com>
2023-02-15 14:18:23 +0200

    code formatted, bugs fixed, minor improvements

--- src/main/java/FindLongestSubstringOf0sAnd1s.java
+++ src/main/java/FindLongestSubstringOf0sAnd1s.java
@@ -1,10 +1,10 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FindLongestSubstringOf0sAnd1s {
-    
+
     public static void main(String args[]) {
-      System.out.println(new FindLongestSubstringOf0sAnd1s().find("1011101101001110101110110100111010111011010011111110111011010011101011101101001110101110110100111") == 11);
+        System.out.println(new FindLongestSubstringOf0sAnd1s().find("1011101101001110101110110100111010111011010011111110111011010011101011101101001110101110110100111") == 11);
     }
 
     // n - a number of characters in the string
@@ -12,10 +12,10 @@ public class FindLongestSubstringOf0sAnd1s {
     // S = O(n)  
     public int find(String s) {
         if (s.length() == 0) return 0;
-        
+
         List<Integer> lengths = new ArrayList<>();
         int count = 0;
-        
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (i == 0 && c == '1') lengths.add(0);
@@ -31,13 +31,13 @@ public class FindLongestSubstringOf0sAnd1s {
                 }
             }
         }
-        
+
         if (lengths.size() == 1) return 1;
-        
+
         int max = 0;
         for (int i = 1; i < lengths.size(); i += 2) {
-            int newLength = 0; 
-            if (i == 1 && lengths.get(i - 1) > 0) {
+            int newLength = 0;
+            if (i == 1 && lengths.get(0) > 0) {
                 newLength = 1;
             } else if (i > 1 && lengths.get(i - 1) == 1) {
                 newLength = 1 + lengths.get(i - 2);
@@ -46,7 +46,7 @@ public class FindLongestSubstringOf0sAnd1s {
             }
             max = Math.max(max, lengths.get(i) + newLength);
         }
-        
+
         return max;
     }
 }
